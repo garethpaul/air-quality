a2175b6fd9ec301056396920789f06a1e9c1a745
Gareth Jones <gareth@garethpaul.com>
2018-11-11 14:56:54 -0800

    changes to make working

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 
-from bottle import route, template, redirect, static_file, error, run
-
+from bottle import route, template, request, response, redirect, static_file, error, run
+from json import dumps
+from test import airquality
 
 @route('/')
 def show_home():
-    return "Hello World!"
+    lat = float(request.query['lat'])
+    lng = float(request.query['lng'])
+    a = airquality(lat, lng).getData()
+    response.content_type = 'application/json'
+    return dumps(a)
 
 if os.environ.get('APP_LOCATION') == 'heroku':
     run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
